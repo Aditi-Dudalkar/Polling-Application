daa3c7a40abc756d47dad4cb355af65487aab4be
Aditi <aditi.dudalkar@gmail.com>
2024-05-15 11:52:25 +0530

    Page creation and rendering

---
+++ src/main/java/com/polling/app/controller/PageController.java
@@ -0,0 +1,39 @@
+package com.polling.app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+import com.polling.app.model.Poll;
+import com.polling.app.service.PollService;
+
+
+@Controller
+public class PageController {
+
+    @Autowired
+    private PollService pollService; 
+
+    @GetMapping("/create-poll")
+    public String showCreatePollPage() {
+        return "create-poll"; // Return the name of the Thymeleaf template without the ".html" extension
+    }
+
+    // @GetMapping("/vote")
+    // public String showVotePage() {
+    //     return "vote"; // Return the name of the Thymeleaf template without the ".html" extension
+    // }
+
+    @GetMapping("/vote")
+    public String showVotePage(Model model) {
+        // Fetch all polls with their options from the database
+        List<Poll> polls = pollService.getAllPolls();
+        model.addAttribute("polls", polls); // Add the polls list to the model
+        return "vote"; // Return the name of the Thymeleaf template without the ".html" extension
+    }
+
+}
+
